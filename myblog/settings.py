d29c946c061599c065e49a4274ca94101073008c
LinjunL <jllcire@gmail.com>
2020-12-04 16:09:00 +1100

    Update settings.py

--- myblog/settings.py
+++ myblog/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
+
+
+…
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
