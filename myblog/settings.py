1ab55e6590af253e2c509efebb23e4258fdcf26c
LJLI <jllcire@gmail.com>
2021-08-04 19:35:06 +1000

    change to panda v1

--- myblog/settings.py
+++ myblog/settings.py
@@ -83,23 +83,23 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#  'default': {
-#      'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#      'NAME': 'backend1',
-#      'USER': 'postgres',
-#      'PASSWORD': '957364',
-#      'HOST': '127.0.0.1',
-#      'PORT': '5432',
-#  }
-# }
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
+  'default': {
+      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+      'NAME': 'backend1',
+      'USER': 'postgres',
+      'PASSWORD': '957364',
+      'HOST': '127.0.0.1',
+      'PORT': '5432',
+  }
 }
+
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+#}
 
 
 # Password validation
