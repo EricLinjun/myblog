d4575b32aaada99f80c7219505de4d052230673c
LJLI <jllcire@gmail.com>
2021-08-12 16:26:08 +1000

    updated heroku

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'g*=rxi22qbz#k1@@eg_g%eip%f4#njthqwxfa$c_dy8c@&wi2w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ericll.herokuapp.com','127.0.0.1']
 
@@ -82,17 +82,17 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-     'default': {
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'backend1',
-       'USER': 'postgres',
-       'PASSWORD': '957364',
-       'HOST': '127.0.0.1',
-       'PORT': '5432',
-     }
- }
+#
+#DATABASES = {
+#     'default': {
+#       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#       'NAME': 'backend1',
+#       'USER': 'postgres',
+#       'PASSWORD': '957364',
+#       'HOST': '127.0.0.1',
+#       'PORT': '5432',
+#     }
+# }
 
 #DATABASES = {
 #  'default': {
